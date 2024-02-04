e5fa85db874fbcd52509770a5c81fca2061d3869
Andrewslayton <andrewsla1726@gmail.com>
2024-02-04 06:14:48 -0500

    please dont be dumb github branches

--- src/app/wrapped/page.tsx
+++ src/app/wrapped/page.tsx
@@ -2,16 +2,16 @@
 
 import MostCommittedRepoSlide from "@/components/MostCommitedRepoSlide";
 import TotalCommitsSlide from "@/components/TotalCommitsSlide";
+import styles from "@/styles/Home.module.css";
 // pages/repositories.tsx
 import { Octokit } from "@octokit/rest";
+import { Event, Streaks } from "@octokit/webhooks-definitions/schema";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import { set } from "zod";
-import React from 'react';
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
 
 
 interface GitHubRepository {
@@ -19,6 +19,11 @@ interface GitHubRepository {
   name: string;
   full_name: string;
   commitCount?: number;
+}
+
+interface Streaks {
+  longestStreak: number;
+  longestBreak: number;
 }
 
 const RepositoriesPage = () => {
@@ -29,7 +34,7 @@ const RepositoriesPage = () => {
   const [commitCount2, setCommitCount2] = useState(0);
   const [highestCommitRepo, setHighestCommitRepo] = useState("");
   const [highestCommitCount, setHighestCommitCount] = useState(0);
-
+  const [streaks, setStreaks] = useState(0);
 
   useEffect(() => {
     const fetchRepositories = async () => {
@@ -61,17 +66,46 @@ const RepositoriesPage = () => {
                 (sum, week) => sum + week.total,
                 0,
               );
-              if(repo.commitCount > highestCommitCount){
+              if (repo.commitCount > highestCommitCount) {
                 highestCommitCount = repo.commitCount;
                 highestCommitRepo = repo.name;
               }
               totalCommits += repo.commitCount;
             } catch (commitError) {
-              console.error(
-                `Error fetching commits for repo ${repo.name}:`,
-                commitError,
-              );
+              // console.error(
+              //   `Error fetching commits for repo ${repo.name}:`,
+              // );
             }
+         try {
+           const repoResponse = await octokit.rest.repos.get({
+             owner: authedUser.data.login,
+             repo: repo.name,
+           });
+
+           if (repoResponse.status === 200) {
+             const commitsResponse = await octokit.rest.repos.listCommits({
+               owner: authedUser.data.login,
+               repo: repo.name,
+             });
+
+             for (const commit of commitsResponse.data) {
+               if (
+                 commit.commit.author != null &&
+                 commit.commit.author.date != null
+               ) {
+                 console.log(
+                   `Commit SHA: ${commit.sha}, Author: ${commit.commit.author.name}, Date: ${commit.commit.author.date}`,
+                 );
+               }
+             }
+           } else {
+             console.log(`Repository ${repo.name} does not exist.`);
+           }
+         } catch (error) {
+           console.error(
+             `Error fetching commits for repository ${repo.name}: ${error}`,
+           );
+         }
           }
           setCommitCount2(totalCommits);
           setHighestCommitRepo(highestCommitRepo);
@@ -113,14 +147,17 @@ const RepositoriesPage = () => {
   }
 
   return (
-      <div className="flex items-center justify-center h-screen bg-black">
+    <div className="flex items-center justify-center h-screen bg-black">
       <div className="w-1/2 mx-auto ">
-      <Slide>
-        <TotalCommitsSlide commitCount2={commitCount2}/>
-        <MostCommittedRepoSlide highestcommited={highestCommitCount} repo={highestCommitRepo}/>
-        {/* Add more slides as needed */}
-      </Slide>
-    </div>
+        <Slide>
+          <TotalCommitsSlide commitCount2={commitCount2} />
+          <MostCommittedRepoSlide
+            highestcommited={highestCommitCount}
+            repo={highestCommitRepo}
+          />
+          {/* Add more slides as needed */}
+        </Slide>
+      </div>
     </div>
   );
 };
